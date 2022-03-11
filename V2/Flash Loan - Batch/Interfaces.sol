cff9880523a43c371d341d21d7a22e8ad2e3fd52
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2022-03-11 18:19:54 +0700

    Update Interfaces.sol

--- V2/Flash Loan - Batch/Interfaces.sol
+++ V2/Flash Loan - Batch/Interfaces.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import { DataTypes } from "Libraries.sol";
+import { DataTypes } from "https://github.com/thanhdatio/FlahloanV2/blob/main/V2/Flash Loan - Batch/Libraries.sol";
 
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
