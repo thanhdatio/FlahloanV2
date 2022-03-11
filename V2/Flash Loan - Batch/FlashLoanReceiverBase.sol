3f8396f6f621b1bda021a359b3daabc36e0bac26
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2022-03-11 18:30:56 +0700

    Update FlashLoanReceiverBase.sol

--- V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
+++ V2/Flash Loan - Batch/FlashLoanReceiverBase.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.6.12;
 
-import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "https://github.com/thanhdatio/FlahloanV2/blob/main/V2/Flash%20Loan%20-%20Batch/Interfaces.sol";
-import { SafeERC20, SafeMath } from "https://github.com/thanhdatio/FlahloanV2/blob/main/V2/Flash%20Loan%20-%20Batch/Libraries.sol";
+import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "./Interfaces.sol";
+import { SafeERC20, SafeMath } from "./Libraries.sol";
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
