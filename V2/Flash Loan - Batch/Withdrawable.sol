7a050ed0c510b9bbef1c1cdd1006a4b1d022ac92
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2022-03-14 13:40:08 +0700

    Update Withdrawable.sol

--- V2/Flash Loan - Batch/Withdrawable.sol
+++ V2/Flash Loan - Batch/Withdrawable.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { SafeERC20, SafeMath } from "./Libraries.sol";
+import "./Ownable.sol";
 
 /**
     Ensures that any contract that inherits from this contract is able to
